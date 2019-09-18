b138bd7b6126f5a72b8d78dcdd7b56d5907787c0
Zhenge Shi <zshi0014@student.monash.edu>
2019-09-18 15:28:43 +1000

    integrated API KEY into the EmailSender.cs

---
+++ FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("noreply@localhost.com", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
